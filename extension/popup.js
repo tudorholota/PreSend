4bbe3c01f48fefca239262087f8c470fe3798353
Holotă Tudor-Matei <tudor.spp12@gmail.com>
2026-08-29 00:13:10 +0300

    privacy-policy update

--- extension/popup.js
+++ extension/popup.js
@@ -25,7 +25,7 @@ async function analyzeMessage(){
 
      
      try {
-        const response = await fetch("http://127.0.0.1:8000/analyze",{
+        const response = await fetch("https://presend.onrender.com/analyze",{
             method : "POST",
             headers :  {"Content-Type": "application/json"},
             body : JSON.stringify({text : text})
